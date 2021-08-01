42b5c802d73c87843aef962c0881e0e1120316a5
TTRCharlie <mcmbazsi@gmail.com>
2021-08-01 17:36:09 +0200

    Change server and client side script's file name

--- __resource.lua
+++ __resource.lua
@@ -2,11 +2,11 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
 	'config.lua',
-    'client1.lua'
+    'client.lua'
 }
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
     'config.lua',
-	'server1.lua'
+	'server.lua'
 }
